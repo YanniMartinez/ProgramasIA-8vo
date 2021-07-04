b8c269912d0e7f951ebb44cfed4834080772b100
Yann <metalyanni@gmail.com>
2021-07-04 11:38:03 -0500

    Código más limpio y eficiente

--- Programa7.py
+++ Programa7.py
@@ -19,27 +19,33 @@ operador = {"V" :"true",
 #****************************************************
 def prepo(listaExp, listaOperadores, listaOperando):
 	""" 
-	
-	@param
-	
+	TODO: El funcionamiento de esta función es separar operandos y operadores en diferentes pilas, una vez
+	que encuentra un ")" comenzará la recursividad y evaluará los casos base. En esta versión no está
+	permitido el valor ¬ aún.
+
+	----------------------------------------------------------------------------------------------
+	Parametros:
+	@param listaExp Hace referencia a la lista con la expresión completa hasta ese momento.
+	@param listaOperadores Hace referencia a la pila de operadores al momento.
+	@param listaOperando Hace referencia a la pila de operandos al momento.
+
+	----------------------------------------------------------------------------------------------
+	Retorno:
+	@return Retorna una cadena donde indica si la expresión es invalida o retorna el valor del resultado de la 
+	expresion.
 	 """
 	resultado = "X Su expresión no está bien formada" #Significa que hubo un error
 	if(len(listaOperadores) > 0 and len(listaOperando)>0):
 		#Hace la operación.
-		print("Entre por recursividad")
-
 		op2 = listaOperadores.pop()
 		operando = listaOperando.pop()
 		op1 = listaOperadores.pop()
 		
 		#Casos particulares:
 		if(operando == "v"):
-			print("Entre a -v- ")
 			if(op1 == "F" and op2 == "F"):
-				print("Sor retorno recursivo")
 				return "F"
 			else:
-				print("Sor retorno recursivo")
 				return "V"
 		#
 		if(operando == "^"):
@@ -66,9 +72,7 @@ def prepo(listaExp, listaOperadores, listaOperando):
 	for elemento in listaExp: #Recorre toda la lista
 		if elemento == ")":
 			#Recursividad
-			print("Soy un ) ="+elemento)
 			resultado = prepo(listaExp, listaOperadores, listaOperando)
-			print(resultado)
 			listaOperadores.append(resultado) #Agregando a operadores
 		
 			
@@ -81,15 +85,12 @@ def prepo(listaExp, listaOperadores, listaOperando):
 			listaOperando.append(elemento)
 
 		elif elemento == "(":
-			#print("Soy un parentesis ( = "+elemento)
-			#listaOperando.append(elemento)
+			#Simplemente deja pasar el simbolo (
 			pass
-		else:
+		else: #Error si encuentra un simbolo incorrecto
 			print("Su expresión no está bien estructurada, el programa finalizará ... ")
 			break
 	return resultado
-	#print(listaOperando)
-	#print(listaOperadores)
 
 #**************************************
 def main():
